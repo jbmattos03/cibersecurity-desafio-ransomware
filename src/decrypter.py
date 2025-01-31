b2ad928f4d45ebaec2c46d4e2b3d6a741496ea01
Julia Brito Mattos <jbmattos2003@gmail.com>
2025-01-31 02:01:49 -0300

    Update decrypter.py

--- src/decrypter.py
+++ src/decrypter.py
@@ -1,17 +1,21 @@
-import os
+mport os
 import pyaes
 
-# Pegar diretório atual do arquivo
+# Pegar diretório atual
 current_directory = os.path.dirname(os.path.abspath(__file__))
 
 # AES
 key = b"testeransomwares"
 aes = pyaes.AESModeOfOperationCTR(key)
 
-# Percorrer árvore de diretórios a partir do diretório atual
+# Percorrer árvore de diretórios
 for root, dirs, files in os.walk(current_directory):
     for file_name in files:
         file_path = os.path.join(root, file_name)
+
+        # Pular arquivos que não têm a extensão .ransomwaretroll
+        if not file_name.endswith(".ransomwaretroll"):
+            continue
         
         # Abrir arquivo a ser encriptado
         with open(file_path, "rb") as file:
@@ -23,7 +27,7 @@ for root, dirs, files in os.walk(current_directory):
         # Encriptar arquivo
         crypto_data = aes.decrypt(file_data)
         
-        # Escrever arquivo encriptado
-        new_file_path = file_path - ".ransomwaretroll"
-        with open(new_file_path, 'wb') as new_file:
+        # Salvar arquivo encriptado
+        new_file_path = file_path[:-len(".ransomwaretroll")]
+        with open(new_file_path, "wb") as new_file:
             new_file.write(crypto_data)
